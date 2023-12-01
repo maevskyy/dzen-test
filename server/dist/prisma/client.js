81b0c6e89049bb1886cd8eb76cc052b97f937296
maevskyy <maevskijdimitrij1@gmail.com>
2023-12-01 07:14:26 +0200

    some fixes in prisma client

--- server/dist/prisma/client.js
+++ server/dist/prisma/client.js
@@ -3,10 +3,6 @@
 const { PrismaClient } = require('@prisma/client');
 
 // Некоторые базовые настройки для типов в Prisma
-global.prisma = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV === "development") {
-  global.prisma = global.prisma;
-}
+global.prisma = new PrismaClient();
 
 module.exports = global.prisma;
